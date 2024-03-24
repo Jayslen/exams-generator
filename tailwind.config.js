fa72e298aac6ee3dec1701dc248e8241ed20a0ab
jayslen <jayslen134@gmail.com>
2024-03-24 01:17:40 -0400

    config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ export default {
       fontFamily: {
         Satoshi: 'Satoshi, sans-serif',
         Sans: 'General Sans, sans-serif'
+      },
+      colors: {
+        chicago: {
+          50: '#f6f5f5',
+          100: '#e8e7e5',
+          200: '#d3d0ce',
+          300: '#b4aeac',
+          400: '#8d8683',
+          500: '#726b68',
+          600: '#5a5552',
+          700: '#524e4c',
+          800: '#474543',
+          900: '#3e3d3b',
+          950: '#282624'
+        }
       }
     }
   },
