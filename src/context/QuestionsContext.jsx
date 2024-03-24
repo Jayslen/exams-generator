e1b2b0f07cca9ef2ae6987f9580e67cbe3fe691e
jayslen <jayslen134@gmail.com>
2024-03-24 01:18:22 -0400

    added function to update the global questions states

--- src/context/QuestionsContext.jsx
+++ src/context/QuestionsContext.jsx
@@ -3,12 +3,23 @@ import { createContext, useState } from 'react'
 export const questionsContext = createContext()
 
 export function QuestionsProvider ({ children }) {
-  const [questions, setQuestions] = useState(['name'])
+  const [questions, setQuestions] = useState([])
 
-  const updateQuestions = (newQuestions) => {
-    setQuestions(newQuestions)
+  const updateQuestions = ({ newQuestions, id }) => {
+    setQuestions((prev) => {
+      if (prev?.questions) {
+        console.log('hay preguntas')
+        return {
+          ...prev,
+          questions: [...newQuestions, ...prev.questions]
+        }
+      }
+      return {
+        questions: [...newQuestions],
+        id
+      }
+    })
   }
-
   return (
     <questionsContext.Provider value={{ questions, updateQuestions }}>
       {children}
