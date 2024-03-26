1bf7cea66adc961f74660f759fb5d64d07e913fb
jayslen <jayslen134@gmail.com>
2024-03-26 00:05:49 -0400

    animaton when click button added

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,9 +1,16 @@
 export function Button ({ handleClick = null, text, type }) {
   return (
     <button
-      className="bg-chicago-300 text-center px-7 py-4 rounded-lg hover:bg-chicago-400 font-Satoshi transition-colors"
+      className="bg-chicago-300 text-center px-7 py-4 rounded-lg hover:bg-chicago-500 hover:text-chicago-100 font-Satoshi transition-colors"
       type={type ?? 'button'}
-      onClick={handleClick}
+      onClick={(e) => {
+        e.target.classList.toggle('animationButton')
+
+        setTimeout(() => {
+          e.target.classList.toggle('animationButton')
+        }, 300)
+        handleClick && handleClick()
+      }}
     >
       {text}
     </button>
