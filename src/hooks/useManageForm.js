a0fbb24a845a64187fc7c87fafbc30dc7308b72c
jayslen <jayslen134@gmail.com>
2024-03-28 18:56:58 -0400

    functions to manage : updatesReviewQuestionsState, checkExam update

--- src/hooks/useManageForm.js
+++ src/hooks/useManageForm.js
@@ -1,32 +1,62 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { ReviewQuestionsContext } from '../context/ReviewQuestionsContext'
 
 export function useManageForm ({ id }) {
   const EXAMS = JSON.parse(window.localStorage.getItem('exams'))
   const CURRENT_EXAM = EXAMS.find((exam) => exam.id === id).questions
 
-  // const [userAnswers, setUserAnswers] = useState([])
+  const { userAnswers, setUserAnswers } = useContext(ReviewQuestionsContext)
   const [currentAnswer, setcurrentAnswer] = useState(null)
   const [currentQuestionIndex, setcurrentQuestionIndex] = useState(0)
   const [isChecked, setIsChecked] = useState(false)
+  const navigate = useNavigate()
 
   const CURRENT_QUESTION = CURRENT_EXAM[currentQuestionIndex]
-  const { question, options } = CURRENT_EXAM[currentQuestionIndex]
+  const { question, options, correctAnswer } = CURRENT_QUESTION
 
-  const CURRENT_ANSWER_IS_CORRECT = (currentAnswer === CURRENT_EXAM[currentQuestionIndex].correctAnswer)
-  const CURRENT_ANSWER_IS_WRONG = (currentAnswer !== CURRENT_EXAM[currentQuestionIndex].correctAnswer && currentAnswer)
+  const CURRENT_ANSWER_IS_CORRECT = currentAnswer === correctAnswer
+  const CURRENT_ANSWER_IS_WRONG = currentAnswer !== correctAnswer && currentAnswer
+
+  const updateUsersAnswers = ({ userAnswer }) => {
+    setUserAnswers((prev) => {
+      return prev?.length > 0
+        ? [...prev, { question, options, userAnswer, correctAnswer, isCorrect: userAnswer === correctAnswer }]
+        : [{ question, options, userAnswer, correctAnswer, isCorrect: userAnswer === correctAnswer }]
+    }
+
+    )
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    const { exam } = Object.fromEntries(new FormData(e.target))
+
+    // go to the review page if the last question is answered
+    if (currentQuestionIndex === CURRENT_EXAM.length - 1 && currentAnswer) {
+      localStorage.setItem('reviews', JSON.stringify(userAnswers))
+      navigate(`/review/${id}`)
+      return
+    }
+
+    // go to next question if the answer is checked
     if (currentAnswer) {
-      setcurrentQuestionIndex(prev => prev + 1)
+      setcurrentQuestionIndex((prev) => prev + 1)
       setcurrentAnswer(null)
-      setIsChecked(prev => !prev)
+      setIsChecked((prev) => !prev)
       e.target.reset()
       return
     }
-    const formData = Object.fromEntries(new FormData(e.target))
-    setcurrentAnswer(formData.exam)
-    setIsChecked(prev => !prev)
+
+    if (!exam) {
+      toast.error('Debes seleccionar una respuesta')
+      return
+    }
+    // check the answer
+    setcurrentAnswer(exam)
+    updateUsersAnswers({ userAnswer: exam })
+    setIsChecked((prev) => !prev)
   }
   return {
     currentQuestionIndex,
@@ -38,7 +68,6 @@ export function useManageForm ({ id }) {
     CURRENT_QUESTION,
     CURRENT_ANSWER_IS_CORRECT,
     CURRENT_ANSWER_IS_WRONG,
-    handleSubmit,
-    setcurrentQuestionIndex
+    handleSubmit
   }
 }
