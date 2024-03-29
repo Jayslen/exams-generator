7cd2fed79f069d96df811ace10044c841afab509
jayslen <jayslen134@gmail.com>
2024-03-29 13:30:05 -0400

    saved the revision of the exam in localStorage

--- src/routes/ExamRevision.jsx
+++ src/routes/ExamRevision.jsx
@@ -1,10 +1,28 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { ReviewQuestionsContext } from '../context/ReviewQuestionsContext'
 import { ReviewedQuestions } from '../components/ReviewedQuestionsComponent'
 // import userAnswers from '../mocks/reviewQuestions.json'
 
 export function ExamRevision () {
-  const { userAnswers } = useContext(ReviewQuestionsContext)
+  const { userAnswers, setUserAnswers } = useContext(ReviewQuestionsContext)
+  const { id } = useParams()
+
+  useEffect(() => {
+    const EXAMS = JSON.parse(localStorage.getItem('exams'))
+    const currentExam = EXAMS.find((exam) => exam.id === id)
+
+    // console.log(exams)
+
+    if (userAnswers.length === 0 && Object.prototype.hasOwnProperty.call(currentExam, 'review')) {
+      setUserAnswers(currentExam.review)
+    }
+
+    if (userAnswers.length > 0) {
+      currentExam.review = userAnswers
+      localStorage.setItem('exams', JSON.stringify([...EXAMS.filter((exam) => exam.id !== id), currentExam]))
+    }
+  }, [])
   return (
     <main>
       <header className='mb-4'>
