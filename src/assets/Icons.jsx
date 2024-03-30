139dc60e790e0cac6f767429b658848b062a43c3
jayslen <jayslen134@gmail.com>
2024-03-29 21:46:30 -0400

    trash icon added

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -39,29 +39,26 @@ export function XIcon ({ width = 24, height = 24 }) {
   )
 }
 
-export function FullScreenIcon () {
+export function DeleteIcon () {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="icon icon-tabler icon-tabler-arrows-maximize"
       width={24}
       height={24}
       viewBox="0 0 24 24"
-      strokeWidth={2}
-      stroke="currentColor"
       fill="none"
+      stroke="currentColor"
+      strokeWidth={1}
       strokeLinecap="round"
       strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-trash"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M16 4l4 0l0 4" />
-      <path d="M14 10l6 -6" />
-      <path d="M8 20l-4 0l0 -4" />
-      <path d="M4 20l6 -6" />
-      <path d="M16 20l4 0l0 -4" />
-      <path d="M14 14l6 6" />
-      <path d="M8 4l-4 0l0 4" />
-      <path d="M4 4l6 6" />
+      <path d="M4 7l16 0" />
+      <path d="M10 11l0 6" />
+      <path d="M14 11l0 6" />
+      <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+      <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
     </svg>
   )
 }
