fc0d671b5f03ff5e0d9bd17bbbe1f964394dfe28
jayslen <jayslen134@gmail.com>
2024-04-27 21:08:29 -0400

    Update LinkComponent to include handleClick prop

--- src/components/LinkComponent.jsx
+++ src/components/LinkComponent.jsx
@@ -1,8 +1,9 @@
-export function LinkComponent ({ text, linkTo = '#' }) {
+export function LinkComponent ({ text, linkTo = '#', handleClick = null }) {
   return (
     <a
       href={linkTo}
       className={`rounded italic font-medium block underline cursor-pointer transition-colors ${linkTo === '#' ? 'cursor-not-allowed text-chicago-300 hover:text-red-500' : 'text-chicago-950 hover:text-chicago-500'}`}
+      onClick={handleClick}
     >
       {text}
     </a>
