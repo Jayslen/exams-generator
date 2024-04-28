f247dd34aab2bf03e2a2ed6b83e7bc03d7753bae
jayslen <jayslen134@gmail.com>
2024-04-27 21:09:45 -0400

    Refactor useManageForm.js for improved functionality and progress tracking

--- src/hooks/useManageForm.js
+++ src/hooks/useManageForm.js
@@ -1,15 +1,23 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { ReviewQuestionsContext } from '../context/ReviewQuestionsContext'
 
 export function useManageForm ({ id }) {
-  const EXAMS = JSON.parse(window.localStorage.getItem('exams'))
-  const CURRENT_EXAM = EXAMS.find((exam) => exam.id === id).questions
-
   const { userAnswers, setUserAnswers } = useContext(ReviewQuestionsContext)
+
+  const EXAMS = JSON.parse(window.localStorage.getItem('exams'))
+  const EXAMS_TRIES = window.localStorage.getItem('tries')
+    ? JSON.parse(window.localStorage.getItem('tries'))
+    : []
+
+  const CURRENT_PROGRESS = EXAMS_TRIES?.find((item) => item.id === id)
+  const CURRENT_EXAM = EXAMS?.find((exam) => exam.id === id).questions
+
   const [currentAnswer, setcurrentAnswer] = useState(null)
-  const [currentQuestionIndex, setcurrentQuestionIndex] = useState(0)
+  const [currentQuestionIndex, setcurrentQuestionIndex] = useState(
+    CURRENT_PROGRESS ? CURRENT_PROGRESS.progress.length : 0
+  )
   const [isChecked, setIsChecked] = useState(false)
   const navigate = useNavigate()
 
@@ -17,15 +25,61 @@ export function useManageForm ({ id }) {
   const { question, options, correctAnswer } = CURRENT_QUESTION
 
   const CURRENT_ANSWER_IS_CORRECT = currentAnswer === correctAnswer
-  const CURRENT_ANSWER_IS_WRONG = currentAnswer !== correctAnswer && currentAnswer
+  const CURRENT_ANSWER_IS_WRONG =
+    currentAnswer !== correctAnswer && currentAnswer
+
+  // update the useranswer if there is a progress saved in the local storage
+  useEffect(() => {
+    if (CURRENT_PROGRESS) {
+      setUserAnswers(CURRENT_PROGRESS.progress)
+    }
+  }, [])
+
+  const generateUserAnswersObj = ({
+    question,
+    options,
+    userAnswer,
+    correctAnswer,
+    isCorrect
+  }) => {
+    return { question, options, userAnswer, correctAnswer, isCorrect }
+  }
 
   const updateUsersAnswers = ({ userAnswer }) => {
     setUserAnswers((prev) => {
       return prev?.length > 0
-        ? [...prev, { question, options, userAnswer, correctAnswer, isCorrect: userAnswer === correctAnswer }]
-        : [{ question, options, userAnswer, correctAnswer, isCorrect: userAnswer === correctAnswer }]
-    }
+        ? [
+            ...prev,
+            generateUserAnswersObj({
+              question,
+              options,
+              userAnswer,
+              correctAnswer,
+              isCorrect: userAnswer === correctAnswer
+            })
+          ]
+        : [
+            generateUserAnswersObj({
+              question,
+              options,
+              userAnswer,
+              correctAnswer,
+              isCorrect: userAnswer === correctAnswer
+            })
+          ]
+    })
+  }
 
+  const clearExamProgress = () => {
+    setUserAnswers([])
+    setcurrentQuestionIndex(0)
+    if (EXAMS_TRIES.length === 0 || EXAMS_TRIES.length === 1) {
+      window.localStorage.removeItem('tries')
+      return
+    }
+    window.localStorage.setItem(
+      'tries',
+      JSON.stringify([...EXAMS_TRIES.filter((item) => item.id !== id)])
     )
   }
 
@@ -35,13 +89,29 @@ export function useManageForm ({ id }) {
 
     // go to the review page if the last question is answered
     if (currentQuestionIndex === CURRENT_EXAM.length - 1 && currentAnswer) {
-      localStorage.setItem('reviews', JSON.stringify(userAnswers))
+      window.localStorage.setItem(
+        'tries',
+        JSON.stringify([...EXAMS_TRIES.filter((item) => item.id !== id)])
+      )
+      window.localStorage.setItem('reviews', JSON.stringify(userAnswers))
       navigate(`/review/${id}`)
       return
     }
 
     // go to next question if the answer is checked
     if (currentAnswer) {
+      // save the progress of the user in the local storage
+      window.localStorage.setItem(
+        'tries',
+        JSON.stringify(
+          !EXAMS_TRIES
+            ? [{ id, progress: userAnswers }]
+            : [
+                ...EXAMS_TRIES.filter((item) => item.id !== id),
+                { id, progress: userAnswers }
+              ]
+        )
+      )
       setcurrentQuestionIndex((prev) => prev + 1)
       setcurrentAnswer(null)
       setIsChecked((prev) => !prev)
@@ -68,6 +138,8 @@ export function useManageForm ({ id }) {
     CURRENT_QUESTION,
     CURRENT_ANSWER_IS_CORRECT,
     CURRENT_ANSWER_IS_WRONG,
+    CURRENT_PROGRESS,
+    clearExamProgress,
     handleSubmit
   }
 }
