fd49e3a858349562f1693780b9b27b20b8d4a0dc
jayslen <jayslen134@gmail.com>
2024-04-29 14:45:09 -0400

    links component move to the top of the page

--- src/routes/ExamRevision.jsx
+++ src/routes/ExamRevision.jsx
@@ -4,28 +4,33 @@ import { LinkComponent } from '../components/LinkComponent'
 import { DetailsComponent } from '../components/DetailsComponent'
 
 export function ExamRevision () {
-  const { userAnswers, id, handleSubmit, notionQuestions } = useSaveReviewQuestion()
+  const { userAnswers, id, handleSubmit, notionQuestions } =
+    useSaveReviewQuestion()
   return (
     <main className="relative">
       <header className="mb-4">
         <h1 className="text-7xl font-black">Revision del examen</h1>
       </header>
 
-      <form
-        onSubmit={handleSubmit}
-      >
-        <DetailsComponent/>
+      <form onSubmit={handleSubmit}>
+        <div className='flex gap-4'>
+          <DetailsComponent />
+          <nav>
+            <LinkComponent text={'Inicio'} linkTo="/" />
+            <LinkComponent text={'Generador de examen'} linkTo="/exam-generator" />
+            <LinkComponent text={'Intentar examen de nuevo'} linkTo={`/exam/${id}`} />
+          </nav>
+        </div>
         {userAnswers.map((value, index) => {
-          return <ReviewedQuestions key={index} questionData={value} databaseQuestions={notionQuestions} />
+          return (
+            <ReviewedQuestions
+              key={index}
+              questionData={value}
+              databaseQuestions={notionQuestions}
+            />
+          )
         })}
       </form>
-      <footer className="flex justify-end gap-2">
-        <LinkComponent text={'Inicio'} linkTo="/" />
-        <LinkComponent
-          text={'Intentar examen de nuevo'}
-          linkTo={`/exam/${id}`}
-        />
-      </footer>
     </main>
   )
 }
