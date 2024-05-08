102085a51cb456628651374ff8406fc148987d84
jayslen <jayslen134@gmail.com>
2024-05-08 18:16:25 -0400

    chore: Update LinkComponent className for disabled links

--- src/components/LinkComponent.jsx
+++ src/components/LinkComponent.jsx
@@ -2,7 +2,7 @@ export function LinkComponent ({ text, linkTo = '#', handleClick = null }) {
   return (
     <a
       href={linkTo}
-      className={`rounded italic font-medium block underline cursor-pointer transition-colors ${linkTo === '#' ? 'cursor-not-allowed text-chicago-300 hover:text-red-500' : 'text-chicago-950 hover:text-chicago-500'}`}
+      className={`rounded italic font-medium block underline cursor-pointer transition-colors ${linkTo === '#' ? 'cursor-not-allowed text-chicago-400 line-through ' : 'text-chicago-950 hover:text-chicago-500'}`}
       onClick={handleClick}
     >
       {text}
